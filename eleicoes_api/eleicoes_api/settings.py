23888f468b36e0599f152db184c0f586adf6ee80
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2026-05-13 15:55:18 -0300

    add configurações do postgres para quando for se conectar, por padr"ao sqlite
    Comment out PostgreSQL database configuration.

--- eleicoes_api/eleicoes_api/settings.py
+++ eleicoes_api/eleicoes_api/settings.py
@@ -81,6 +81,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+         ## 'ENGINE': 'django.db.backends.postgresql',
+         ##  'NAME': 'urna_db',
+       ## 'USER': 'postgres',
+       ## 'PASSWORD': 'postgres',
+       ## 'HOST': 'localhost',
+       ## 'PORT': '5432',
     }
 }
 
